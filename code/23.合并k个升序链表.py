d4b6afc93af6f92663295a257d4264ebe3547d2d
LL <841935390@qq.com>
2021-09-07 23:47:54 +0800

    排序

---
+++ code/23.合并k个升序链表.py
@@ -0,0 +1,91 @@
+#
+# @lc app=leetcode.cn id=23 lang=python3
+#
+# [23] 合并K个升序链表
+#
+# https://leetcode-cn.com/problems/merge-k-sorted-lists/description/
+#
+# algorithms
+# Hard (55.85%)
+# Likes:    1498
+# Dislikes: 0
+# Total Accepted:    315.2K
+# Total Submissions: 564K
+# Testcase Example:  '[[1,4,5],[1,3,4],[2,6]]'
+#
+# 给你一个链表数组，每个链表都已经按升序排列。
+# 
+# 请你将所有链表合并到一个升序链表中，返回合并后的链表。
+# 
+# 
+# 
+# 示例 1：
+# 
+# 输入：lists = [[1,4,5],[1,3,4],[2,6]]
+# 输出：[1,1,2,3,4,4,5,6]
+# 解释：链表数组如下：
+# [
+# ⁠ 1->4->5,
+# ⁠ 1->3->4,
+# ⁠ 2->6
+# ]
+# 将它们合并到一个有序链表中得到。
+# 1->1->2->3->4->4->5->6
+# 
+# 
+# 示例 2：
+# 
+# 输入：lists = []
+# 输出：[]
+# 
+# 
+# 示例 3：
+# 
+# 输入：lists = [[]]
+# 输出：[]
+# 
+# 
+# 
+# 
+# 提示：
+# 
+# 
+# k == lists.length
+# 0 <= k <= 10^4
+# 0 <= lists[i].length <= 500
+# -10^4 <= lists[i][j] <= 10^4
+# lists[i] 按 升序 排列
+# lists[i].length 的总和不超过 10^4
+# 
+# 
+#
+
+# @lc code=start
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeKLists(self, lists: List[ListNode]) -> ListNode:
+        if not lists:return 
+        n = len(lists)
+        return self.merge(lists, 0, n-1)
+    def merge(self,lists, left, right):
+        if left == right:
+            return lists[left]
+        mid = left + (right - left) // 2
+        l1 = self.merge(lists, left, mid)
+        l2 = self.merge(lists, mid+1, right)
+        return self.mergeTwoLists(l1, l2)
+    def mergeTwoLists(self,l1, l2):
+        if not l1:return l2
+        if not l2:return l1
+        if l1.val < l2.val:
+            l1.next = self.mergeTwoLists(l1.next, l2)
+            return l1
+        else:
+            l2.next = self.mergeTwoLists(l1, l2.next)
+            return l2
+# @lc code=end
+
