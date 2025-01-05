4969bab2803c907fb12374a2e90a75cf5b0e6284
liling <841935390@qq.com>
2025-01-05 16:02:24 +0800

    2025-01-05 15:55:37: ...
    Affected files:
    .obsidian/workspace.json
    code/111.二叉树的最小深度.py
    explain/104. 二叉树的最大深度_E.md
    explain/111. 二叉树的最小深度.md
    原理/12【数据结构与算法】二叉树.md
    原理/16【数据结构与算法】深度和广度优先搜索.md

---
+++ code/111.二叉树的最小深度.py
@@ -0,0 +1,62 @@
+#
+# @lc app=leetcode.cn id=111 lang=python3
+#
+# [111] 二叉树的最小深度
+#
+# https://leetcode.cn/problems/minimum-depth-of-binary-tree/description/
+#
+# algorithms
+# Easy (54.37%)
+# Likes:    1250
+# Dislikes: 0
+# Total Accepted:    763.6K
+# Total Submissions: 1.4M
+# Testcase Example:  '[3,9,20,null,null,15,7]'
+#
+# 给定一个二叉树，找出其最小深度。
+# 
+# 最小深度是从根节点到最近叶子节点的最短路径上的节点数量。
+# 
+# 说明：叶子节点是指没有子节点的节点。
+# 
+# 
+# 
+# 示例 1：
+# 
+# 
+# 输入：root = [3,9,20,null,null,15,7]
+# 输出：2
+# 
+# 
+# 示例 2：
+# 
+# 
+# 输入：root = [2,null,3,null,4,null,5,null,6]
+# 输出：5
+# 
+# 
+# 
+# 
+# 提示：
+# 
+# 
+# 树中节点数的范围在 [0, 10^5] 内
+# -1000 
+# 
+# 
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def minDepth(self, root: Optional[TreeNode]) -> int:
+        pass
+        
+# @lc code=end
+
