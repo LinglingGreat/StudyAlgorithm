752d84d9719faa623739e22e130044328183743f
liling <841935390@qq.com>
2025-01-05 20:07:35 +0800

    2025-01-05 19:20:33: ...
    Affected files:
    .obsidian/workspace.json
    code/1836.从未排序的链表中移除重复元素.py
    code/204.计数质数.py
    code/264.丑数-ii.py
    code/82.删除排序链表中的重复元素-ii.py
    explain/1836. 从未排序的链表中移除重复元素.md
    explain/264. 丑数 II.md
    explain/82. 删除排序链表中的重复元素 II.md
    原理/链表.md

---
+++ code/204.计数质数.py
@@ -0,0 +1,57 @@
+#
+# @lc app=leetcode.cn id=204 lang=python3
+#
+# [204] 计数质数
+#
+# https://leetcode.cn/problems/count-primes/description/
+#
+# algorithms
+# Medium (37.01%)
+# Likes:    1199
+# Dislikes: 0
+# Total Accepted:    295K
+# Total Submissions: 797.5K
+# Testcase Example:  '10'
+#
+# 给定整数 n ，返回 所有小于非负整数 n 的质数的数量 。
+# 
+# 
+# 
+# 示例 1：
+# 
+# 
+# 输入：n = 10
+# 输出：4
+# 解释：小于 10 的质数一共有 4 个, 它们是 2, 3, 5, 7 。
+# 
+# 
+# 示例 2：
+# 
+# 
+# 输入：n = 0
+# 输出：0
+# 
+# 
+# 示例 3：
+# 
+# 
+# 输入：n = 1
+# 输出：0
+# 
+# 
+# 
+# 
+# 提示：
+# 
+# 
+# 0 <= n <= 5 * 10^6
+# 
+# 
+#
+
+# @lc code=start
+class Solution:
+    def countPrimes(self, n: int) -> int:
+        
+# @lc code=end
+
---
+++ code/264.丑数-ii.py
@@ -0,0 +1,53 @@
+#
+# @lc app=leetcode.cn id=264 lang=python3
+#
+# [264] 丑数 II
+#
+# https://leetcode.cn/problems/ugly-number-ii/description/
+#
+# algorithms
+# Medium (58.18%)
+# Likes:    1241
+# Dislikes: 0
+# Total Accepted:    192K
+# Total Submissions: 330.2K
+# Testcase Example:  '10'
+#
+# 给你一个整数 n ，请你找出并返回第 n 个 丑数 。
+# 
+# 丑数 就是质因子只包含 2、3 和 5 的正整数。
+# 
+# 
+# 
+# 示例 1：
+# 
+# 
+# 输入：n = 10
+# 输出：12
+# 解释：[1, 2, 3, 4, 5, 6, 8, 9, 10, 12] 是由前 10 个丑数组成的序列。
+# 
+# 
+# 示例 2：
+# 
+# 
+# 输入：n = 1
+# 输出：1
+# 解释：1 通常被视为丑数。
+# 
+# 
+# 
+# 
+# 提示：
+# 
+# 
+# 1 <= n <= 1690
+# 
+# 
+#
+
+# @lc code=start
+class Solution:
+    def nthUglyNumber(self, n: int) -> int:
+        
+# @lc code=end
+
